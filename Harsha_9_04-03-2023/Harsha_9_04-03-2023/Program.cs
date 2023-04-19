91e68d82d879bf6ea9fab0374aa341b0e309a3c1
Shravan Kumar Bandigari <sbandigari@ariqt.com>
2023-04-19 16:13:17 +0530

    Added Logical question 8, 9 and 10

---
+++ Harsha_9_04-03-2023/Harsha_9_04-03-2023/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+int[] a = { 0, 1, 1 };
+int[] b = { 1, -10, 10 };
+//int[] diffrence = new int[a.Length];
+
+//for (int i = 0; i < a.Length; i++)
+//{
+//    int v1=0, v2=0;
+//    if (a[i]<0)
+//    {
+//        v1 = (-a[i]);
+//    }
+//    else
+//    {
+//        v1 =a[i];
+//    }
+//    if (b[i]<0)
+//    {
+//        v2 = v1-b[i];
+//    }
+//    else
+//    {
+//        v2 = v1+b[i];
+//    }
+//    diffrence[i] = v2;
+//}
+int maxDiff = int.MinValue;
+
+//int i = 0;
+int index = 0;
+
+
+for (int i = 0; i < a.Length ; i++)
+{
+    int gapValue = Math.Abs(a[i] - b[i]);
+    //int var = gapValue.Max();
+
+    if (gapValue > maxDiff)
+    {
+        maxDiff = gapValue;
+       
+        index = i;
+    }
+}
+
+Console.WriteLine(index);
+//int a =IndexOf( diffrence.Max());
+
+//int index = Array.IndexOf(diffrence, var);
+
+//Console.WriteLine(index);
+
+
+//for (int i = 0; i < a.Length; i++)
+//{
+//    diffrence[i]=a[i]-b[i];
+//}
+//Console.WriteLine(diffrence.Max());
---
+++ Harsha_Assessment_8_04-03-2023/Harsha_Assessment_8_04-03-2023/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+//Stopwatch sw= Stopwatch.StartNew();
+try
+{
+    Console.WriteLine("Enter a number to get its Highest value ");
+    int number = int.Parse(Console.ReadLine());
+
+    //int start = 1;
+    int factor = 1;
+    for (int i = 2; i<=number/2; i++)
+    {
+        if (number%i==0)
+        {
+            factor=i;
+        }
+    }
+    Console.WriteLine(factor);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
